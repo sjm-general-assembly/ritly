5326e4de4b9d99f9487713514db08d25eebe475b
smarsh25 <sjm.atl@gmail.com>
2014-01-26 13:23:39 -0800

    refactor rlinks_controller, to remove extra id = params[:id] lines

--- app/controllers/rlinks_controller.rb
+++ app/controllers/rlinks_controller.rb
@@ -19,25 +19,29 @@ class RlinksController < ApplicationController
 
 		# GETs a form (disabled) to display a given link
 		def show
-			id = params[:id]
-			@current_link = Rlink.find(id)
-			@on_show_page = true
-			@on_show_item_id = id
+			@current_link = Rlink.find(params[:id])
 		end
 
 		# GETs a form to edit a link
 		def edit
-			id = params[:id]
-			@rlink = Rlink.find(id)
+			@rlink = Rlink.find(params[:id])
 		end
 
 		# PATCH saves the updated attributes
 		def update
-			id = params[:id]
 			updated_info = params.require(:rlink).permit(:link, :random_string)
-			rlink = Rlink.find(id)
+			rlink = Rlink.find(params[:id])
 			rlink.update_attributes(updated_info)
 			redirect_to rlink
 		end
 
+		# DELETE destroy the selected link
+		def destroy
+			rlink = Rlink.find(params[:id])
+			rlink.destroy
+
+			redirect_to action: :index
+		end
+
 end
+
