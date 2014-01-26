a1bb560b3b07608865857650b0b5549408a38bb0
smarsh25 <sjm.atl@gmail.com>
2014-01-26 14:36:27 -0800

    adding first version of random_string/token lookup, is working, need to add error handling

--- app/models/rlink.rb
+++ app/models/rlink.rb
@@ -2,10 +2,11 @@ class Rlink < ActiveRecord::Base
 	validates :random_string, uniqueness: true
 
 	def self.url_lookup_by_token(link_token)
-		puts("****** link token: #{link_token}")
 
-		# new_url = Rlink.lookup_token(link_token)
-		lookup_url = 'www.google.com'
+		# search for the record with provided link token (aka random_string)
+		lookup_url = self.find_by(random_string: link_token).link
+
+		# TODO - consider adding index on random_string, improve search performance
 
 		# check url, prepend scheme if missing
 		lookup_url.prepend('http://') if URI(lookup_url).scheme == nil
