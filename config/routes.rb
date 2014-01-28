79b09ecf44e333143ee0d277201bf113d4721665
smarsh25 <sjm.atl@gmail.com>
2014-01-27 16:29:09 -0800

    refactoring edit and show views to use bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -19,23 +19,3 @@ Ritly::Application.routes.draw do
 	get '/rlinks/go/:link_token', to: "rlinks#route_link"
 
 end
-
-# just wanted to share this code in case anyone wants to use it. 
-# There's a URI method you can call on a link <URI(link)> that will
-# let you check the different parts of it, so could check to see if
-# it has 'http' in the beginning by calling <link.scheme>. Before my
-# app would only work if you gave it a link that already contained
-# 'http', but this lets you test to see if it's not there and you can
-# add it in for the user.
-
-# def route
-#     ritlycode = params[:ritlyurl]
-#     url = Url.find_by string: ritlycode
-#     link = url.link
-
-#     test = URI(link)
-#     link = "http://" + link if test.scheme == nil
-
-#     redirect_to link
-#   end
-
