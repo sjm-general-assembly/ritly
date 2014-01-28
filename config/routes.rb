31e7cdb6f227bb10f419b900622f9237a95924a8
smarsh25 <sjm.atl@gmail.com>
2014-01-27 21:35:35 -0800

    generating new controllers for user auth, moving _nav_bar to work for multiple controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Ritly::Application.routes.draw do
 
-	# create a route for root link, sending to main/home page (index)
-	root to: 'rlinks#index'
-
+  get "users/new"
+  get "users/show"
+  get "users/create"
+  get "sessions/new"
+  get "sessions/destroy"
+  get "new/show"
+  get "new/create"
 	# all standard Restful CRUD routes
 	#      root GET     /                          rlinks#index
 	#     rlinks GET    /rlinks(.:format)          rlinks#index
@@ -15,7 +19,16 @@ Ritly::Application.routes.draw do
 	#            DELETE /rlinks/:id(.:format)      rlinks#destroy
 	resources	:rlinks
 
+	# create a route for root link, sending to main/home page (index)
+	root to: 'rlinks#index'
+
 	# custom application routes
 	get '/rlinks/go/:link_token', to: "rlinks#route_link"
+
+	# list routes for user login logout
+	get '/signup' => 'users#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  get'/signin' => 'sessions#new'
+
 
 end
